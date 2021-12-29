dc2ada05ed7cd8d6eb4703287ab14c24db81514e
Calen-Ray <calen.w.ray@gmail.com>
2021-12-29 14:18:28 -0800

    finished v1

--- django_helper.py
+++ django_helper.py
@@ -4,6 +4,9 @@ import re
 
 # all file contents are stored seperately to maintain readable code. 
 from djang_helper_supporting import project_url_contents, settings_file_contents
+from djang_helper_supporting import css_content, vue_content, html_index_content, html_base_content
+from djang_helper_supporting import api_urls_content, api_serializer_content, api_views_content
+from djang_helper_supporting import users_forms_content, users_models_content, users_views_content, users_urls_content
 
 
 # for now these are hard-coded but the intention is to allow the user to chnage the name of any of these.
@@ -40,8 +43,71 @@ with open('settings.py', 'r') as projects_settings_file:
 
 print(f'modifying settings.py project file to include new apps and DRF settings as well as replacing the secret key.')
 with open('settings.py', 'w') as projects_settings_file:
-    projects_settings_file.write(re.sub('SECRET_KEY = .*',saved_secret_key,settings_file_contents.format(custom_app,api_app,user_app,time_zone)))
+    projects_settings_file.write(re.sub('SECRET_KEY = .*',saved_secret_key,settings_file_contents.format(custom_app,api_app,user_app,project_name,project_name,time_zone)))
     print(f'injected oriignal {saved_secret_key}')
 
 
 
+
+# With the project level work done, we can move onto the apps, starting with users.
+os.chdir(f'../{user_app}')
+
+print(f'modifying users app..')
+
+print(f'modifying users models.py..')
+with open('models.py', 'w') as users_models_file:
+    users_models_file.write(users_models_content)
+
+print(f'modifying users views.py..')
+with open('views.py', 'w') as users_views_file:
+    users_views_file.write(users_views_content)
+
+print(f'modifying users forms.py..')
+with open('forms.py', 'w') as users_forms_file:
+    users_forms_file.write(users_forms_content)
+
+print(f'modifying users urls.py..')
+with open('urls.py', 'w') as users_urls_file:
+    users_urls_file.write(users_urls_content)
+
+
+# Next app will be the api.
+os.chdir(f'../{api_app}')
+
+print(f'modifying api app..')
+
+print(f'modifying api views.py..')
+with open('views.py', 'w') as api_views_file:
+    api_views_file.write(api_views_content)
+
+print(f'modifying api serializers.py..')
+with open('serializers.py', 'w') as api_serializer_file:
+    api_serializer_file.write(api_serializer_content.format(user_app))
+
+print(f'modifying api urls.py..')
+with open('urls.py', 'w') as api_urls_file:
+    api_urls_file.write(api_urls_content)
+
+
+
+# lets build the template and static dirs now.. 
+os.chdir('..')
+os.mkdir('templates')
+os.mkdir('static')
+
+with open('templates/base.html', 'w') as html_file:
+    html_file.write(html_base_content)
+
+with open('templates/home.html', 'w') as html_file:
+    html_file.write(html_index_content)
+
+with open('static/app.js', 'w') as vue_file:
+    vue_file.write(vue_content)
+
+with open('static/style.css', 'w') as css_file:
+    css_file.write(css_content)
+
+# management.call_command('makemigrations', user_app)
+# management.call_command('migrate', user_app)
+management.call_command('makemigrations')
+management.call_command('migrate')
